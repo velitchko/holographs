e16e6fdaceab2e1808f322786b313522138a7d94
Velitchko Filipov <velitchko@users.noreply.github.com>
2024-04-12 12:49:43 +0200

    setup app html and dependencies

--- holographs/src/app/app.module.ts
+++ holographs/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphComponent } from '../components/graph/graph.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GraphComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
